8b9b1ea2408571cbac32587eb1b2b99d4a2cc49b
eenxn <kobaluxx@gmail.com>
2026-01-10 10:33:40 +0700

    ไว้มาทำต่อ โจทย์เหลี่ยม

--- codebreaker/problemsetter.cpp
+++ codebreaker/problemsetter.cpp
@@ -15,27 +15,30 @@ int32_t main()
 
     for(int i=0; i<c; i++) cin>>contest[i].first>>contest[i].second;
     for(int i=0; i<p; i++) cin>>problem[i].first>>problem[i].second;
-
+    
     sort(contest.begin(), contest.end());
+    
+    vector<int> pref;
+    int temp = contest[0].second;
+    pref.push_back(contest[0].second);
 
-    int mx;
+    for(int i=1; i<c; i++)
+    {
+        temp += contest[i].second;
+        pref.push_back(temp);
+    }
+
     for(int i =0; i<p; i++)
     {
         //debug(i)
-        mx = 0;
         auto it = upper_bound(contest.begin(), contest.end(), make_pair(problem[i].first, INF));
-        int index = it- contest.begin()-1;
-            // if(problem[i].first >= contest[j].first)
-            // {
-            //     int cur = contest[j].second-problem[i].second;
-            //     if(cur>mx and cur >=0)
-            //     {
-            //         mx = cur;
-            //         //debug(mx)
-            //     }
-            // }
-            
-        sat += mx;
+        int index = it- contest.begin()-1; //debug(index)
+
+        auto it2 = upper_bound(pref.begin(), pref.begin()+index, contest[index].second); //debug(contest[index].second);
+        int idx = it2-pref.begin(); //debug(idx)
+
+        int cursat= contest[idx].second-problem[i].second;//debug(cursat)
+        if(cursat>=0) sat += (cursat); 
         //debug(sat)
     }
 
